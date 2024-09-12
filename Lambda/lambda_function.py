1d0418250cdadbdd95795aa9bbfb701ae2c774ef
Samuel Ackah <sackah@ucop.edu>
2024-09-11 22:30:36 -0700

    Updated 7

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello from Lamb!')
+    'body': json.dumps('Hello from Lambda now!')
   }
